3ba8fc67698d3b57a9ec0339dd718a1b5166bc74
Vikram Pratap Singh <vikram.singh@forethought.in>
2023-02-06 13:16:27 +0530

    Vk Component added

--- src/Vk.js
+++ src/Vk.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Vk() {
   return (
     <div className="App">
       <header className="App-header">
-       App Hello Package
+      Vk Hello Package
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -23,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default Vk;
